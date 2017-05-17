805c08c0b1e320ef9001fd4b126c2de7eae438b9
Sorion <maxime.pelte@gmail.com>
2017-05-17 14:57:12 +0200

    Adding app constant class, adding app data path const

---
+++ AppConst.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSecure
+{
+    public static class AppConst
+    {
+        // This must be use with Environment.ExpandEnvironmentVariables() to have the full path
+        public static readonly string APP_DATA_FOLDER = "%AppData%\\MailSecure";
+    }
+}
