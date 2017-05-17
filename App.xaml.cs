b0eb982876edd7f032d631672d83569d2e452681
Sorion <maxime.pelte@gmail.com>
2017-05-17 14:45:17 +0200

    Adding Server Const in class, changing in code port to use static class

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace MailSecure {
+namespace MailSecure
+{
     /// <summary>
     /// Logique d'interaction pour App.xaml
     /// </summary>
@@ -14,7 +9,7 @@ namespace MailSecure {
         public static MailSender mailSender { get; set; }
 
         public App() {
-            mailSender = new MailSender("smtp-mail.outlook.com", 587);
+            mailSender = new MailSender("smtp-mail.outlook.com", ServerFactConst.DEFAULT_SMTP_PORT_TLS);
         }
     }
 }
