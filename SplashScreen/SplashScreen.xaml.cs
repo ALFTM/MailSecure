425dcc145bfc844a5d8670e8403dcd63259b80de
Sorion <maxime.pelte@gmail.com>
2017-05-28 20:19:55 +0200

    Adding SplashScreen Window

---
+++ SplashScreen/SplashScreen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MailSecure.SplashScreen
+{
+    /// <summary>
+    /// Logique d'interaction pour SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
