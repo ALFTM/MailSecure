da80c91d3a7b9069a7799099adad9bd80fbf9a5a
Sorion <maxime.pelte@gmail.com>
2017-07-07 16:50:23 +0200

    Adding a richBox With Text decoration

---
+++ MailSecure/Controls/RichTextBoxControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace MailSecure
+{
+    /// <summary>
+    /// Logique d'interaction pour RichTextBoxControl.xaml
+    /// </summary>
+    public partial class RichTextBoxControl : UserControl
+    {
+        public RichTextBoxControl()
+        {
+            InitializeComponent();
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            
+            object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            //cmbFontFamily.SelectedItem = temp;
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            //cmbFontSize.Text = temp.ToString();
+        }
+    }
+}
