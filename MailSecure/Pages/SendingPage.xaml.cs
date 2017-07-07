48b57850d553a11be5c1e0735a56e8a52c26eb30
Sorion <maxime.pelte@gmail.com>
2017-07-07 14:18:42 +0200

    Adding Sending Page

---
+++ MailSecure/Pages/SendingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MailSecure
+{
+    /// <summary>
+    /// Logique d'interaction pour SendingPage.xaml
+    /// </summary>
+    public partial class SendingPage : Page
+    {
+        public SendingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
