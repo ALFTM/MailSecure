299229bdd9af6540ff9f18657eae07d02b5d623c
Sorion <maxime.pelte@gmail.com>
2017-07-07 16:05:24 +0200

    Adding button to control cc cci

---
+++ MailSecure/Converter/VisibilityToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace MailSecure
+{
+    class VisibilityToColorConverter : BaseConverter<VisibilityToColorConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Visibility)value == Visibility.Visible) {
+                return App.Current.FindResource("SecondaryBrush");
+            }
+
+            return App.Current.FindResource("BackgroundBrush");
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
