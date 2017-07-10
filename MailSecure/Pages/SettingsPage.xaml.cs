676c65211fb734e1914227ccdbb7b6efff8f3dfd
Sorion <maxime.pelte@gmail.com>
2017-07-10 16:37:17 +0200

    Navigate through page

---
+++ MailSecure/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace MailSecure
+{
+    /// <summary>
+    /// Logique d'interaction pour Settings.xaml
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
