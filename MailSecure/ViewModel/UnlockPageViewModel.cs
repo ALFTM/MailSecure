14627767ed5aa5094f13cc7c090909d0f7dec415
Sorion <maxime.pelte@gmail.com>
2017-07-13 16:00:23 +0200

    Display a simple list of imap

--- MailSecure/ViewModel/UnlockPageViewModel.cs
+++ MailSecure/ViewModel/UnlockPageViewModel.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MailSecure.Core;
+﻿using MailSecure.Core;
+using System.Collections.ObjectModel;
 using System.Net.Mail;
 
 namespace MailSecure
 {
     class UnlockPageViewModel : BaseViewModel
     {
+        #region Public Members
+        public ObservableCollection<MailMessage> ImapList { get; set; }
+        #endregion
+
         #region Constructor
         public UnlockPageViewModel()
         {
+            ImapList = new ObservableCollection<MailMessage>();
             var mailReceiver = new MailReceiver(App.CurrentUserData.CurrentUser);
             mailReceiver.PrepareImap();
 
@@ -21,6 +22,8 @@ namespace MailSecure
             foreach(MailMessage mailMessage in listMessage)
             {
                 var from = mailMessage.From;
+
+                ImapList.Add(mailMessage);
             }
         }
         #endregion
