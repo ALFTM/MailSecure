8d0db00841cca7136c134efa9eb8154ceb48bdb1
Sorion <maxime.pelte@gmail.com>
2017-07-14 12:55:48 +0200

    Adding Hover on List, use control

--- MailSecure/Converter/BoolToColorConverter.cs
+++ MailSecure/Converter/BoolToColorConverter.cs
@@ -8,7 +8,7 @@ namespace MailSecure
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if ((Boolean)value == true) {
-                return App.Current.FindResource("SecondaryDarkBrush");
+                return App.Current.FindResource("SecondaryLightBrush");
             }
 
             return App.Current.FindResource("BackgroundBrush");
