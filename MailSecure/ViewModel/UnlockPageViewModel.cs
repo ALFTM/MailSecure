23f751d6f41c87043da74a417670f0c0dab82c56
teddy.reino9@gmail.com <teddy.reino9@gmail.com>
2017-07-14 18:24:37 +0200

    Message Method in async

--- MailSecure/ViewModel/UnlockPageViewModel.cs
+++ MailSecure/ViewModel/UnlockPageViewModel.cs
@@ -1,30 +1,63 @@
 ﻿using MailSecure.Core;
 using System.Collections.ObjectModel;
 using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace MailSecure
 {
     class UnlockPageViewModel : BaseViewModel
     {
+        #region Private Members
+        private string loadingIsVisible;
+        private string imapListIsVisible;
+        #endregion
+
         #region Public Members
         public ObservableCollection<MailMessage> ImapList { get; set; }
+        public string LoadingIsVisible
+        {
+            get => loadingIsVisible;
+            set
+            {
+                loadingIsVisible = value;
+                OnPropertyChanged(nameof(loadingIsVisible));
+            }
+        }
+        public string ImapListIsVisible
+        {
+            get => imapListIsVisible;
+            set
+            {
+                imapListIsVisible = value;
+                OnPropertyChanged(nameof(imapListIsVisible));
+            }
+        }
         #endregion
 
         #region Constructor
         public UnlockPageViewModel()
         {
             ImapList = new ObservableCollection<MailMessage>();
-            var mailReceiver = new MailReceiver(App.CurrentUserData.CurrentUser);
+            FullFillMessageList(App.CurrentUserData.CurrentUser);
+        }
+        #endregion
+
+        #region Private Methods
+        private async void FullFillMessageList(UserMailFacts crtUser)
+        {
+            var mailReceiver = new MailReceiver(crtUser);
             mailReceiver.PrepareImap();
 
-            var listMessage = mailReceiver.GetMessagesHeader();
+            var listMessage = await Task.Run(() => mailReceiver.GetMessagesHeader());
 
-            foreach(MailMessage mailMessage in listMessage)
+            foreach (MailMessage mailMessage in listMessage)
             {
                 var from = mailMessage.From;
 
                 ImapList.Add(mailMessage);
             }
+            LoadingIsVisible = "false";
+            ImapListIsVisible = "true";
         }
         #endregion
     }
