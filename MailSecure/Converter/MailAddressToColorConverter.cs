dcc0c67cfb6ffad79142c9f6b101ded6a4300a0c
Sorion <maxime.pelte@gmail.com>
2017-07-14 12:46:37 +0200

    Updating list style

---
+++ MailSecure/Converter/MailAddressToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Mail;
+
+namespace MailSecure
+{
+    class MailAddressToColorConverter : BaseConverter<MailAddressToColorConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var from = (MailAddress)value;
+            char letter = from.User[0];
+
+            if(letter >= 97 && letter <= 102) {
+                return App.Current.FindResource("InitialsAToFBrush");
+            }
+
+            if (letter > 102 && letter <= 110) {
+                return App.Current.FindResource("InitialsGToNBrush");
+            }
+
+            return App.Current.FindResource("InitialsOToZBrush");
+
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
