6f99c853ea12d1478048ab5088ab50d1751922d4
Sorion <maxime.pelte@gmail.com>
2017-07-16 01:06:05 +0200

    Adding list of attachments

---
+++ MailSecure/Controls/Unlock/AttachemenUnlockListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MailSecure
+{
+    /// <summary>
+    /// Logique d'interaction pour AttachementListControl.xaml
+    /// </summary>
+    public partial class AttachementUnlockListControl : UserControl
+    {
+        public AttachementUnlockListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
