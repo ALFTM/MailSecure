e80527b93bacc5d8ba2245fc07015b05f860eaff
Sorion <maxime.pelte@gmail.com>
2017-07-16 02:12:08 +0200

    disable button if no password

---
+++ MailSecure/Converter/BoolToInvertBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace MailSecure
+{
+    class BoolToInvertBoolConverter : BaseConverter<BoolToInvertBoolConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Boolean)value == true) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
