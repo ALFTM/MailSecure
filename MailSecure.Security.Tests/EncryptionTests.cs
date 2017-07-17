b79fbc9cc3669615f3a9b03f3b54a1eda0a38ebf
teddy.reino9@gmail.com <teddy.reino9@gmail.com>
2017-07-17 18:35:41 +0200

    Add two tests

---
+++ MailSecure.Security.Tests/EncryptionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSecure.Security.Tests
+{
+    [TestClass]
+    public class EncryptionTests
+    {
+        [TestMethod]
+        public void EncryptString()
+        {
+            // Arrange
+            var exceptedResult = "i'm a test";
+            var password = "password";
+
+            // Act
+            var res = Encryption.Encrypt(exceptedResult, password);
+            Assert.AreNotEqual(exceptedResult, res);
+            res = Encryption.Decrypt(res, password);
+
+            // Assert
+            Assert.AreEqual(exceptedResult, res);
+        }
+
+        [TestMethod]
+        public void EncryptTwoStringAreDifferent()
+        {
+            // Arrange
+            var exceptedResult = "i'm a test";
+            var password = "password";
+
+            // Act
+            var firstRes = Encryption.Encrypt(exceptedResult, password);
+            var secondRes = Encryption.Encrypt(exceptedResult, password);
+
+            // Assert
+            Assert.AreNotEqual(firstRes, secondRes);
+        }
+    }
+}
