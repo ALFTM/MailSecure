ede574de085dc8fa4cd02a4301d57df97297648f
teddy.reino9@gmail.com <teddy.reino9@gmail.com>
2017-07-19 22:04:25 +0200

    Password generated VM & message when wrong password

---
+++ MailSecure/ViewModel/PasswordPopupViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSecure.Core;
+
+namespace MailSecure
+{
+    class PasswordPopupViewModel : BaseViewModel
+    {
+        #region ContentLanguage
+        public string PasswordPopupTitleLbl
+        {
+            get => App.ApplicationLanguage.GetStringFromLanguage("passwordPopupTitle_lbl");
+        }
+
+        public string CopyLbl
+        {
+            get => App.ApplicationLanguage.GetStringFromLanguage("copy_lbl");
+        }
+        #endregion
+
+        #region Constructor
+        public PasswordPopupViewModel()
+        {
+
+        }
+        #endregion
+    }
+}
