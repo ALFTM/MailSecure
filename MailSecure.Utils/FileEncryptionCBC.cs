cb9f5b7cc10ac803d98bab9184a908dbebb60a6d
Sorion <maxime.pelte@gmail.com>
2017-10-09 15:57:38 +0200

    Adding FileEncryption Interface, stating implementing fileEncryption with CBC

---
+++ MailSecure.Utils/FileEncryptionCBC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSecure.Security
+{
+    class FileEncryptionCBC : IFileEncryption
+    {
+        public void DecryptFile(string sourceFilename, string destinationFilename, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EncryptFile(string sourceFilename, string destinationFilename, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MailSecure.Utils/IFileEncryption.cs
@@ -0,0 +1,8 @@
+﻿namespace MailSecure.Security
+{
+    interface IFileEncryption
+    {
+        void EncryptFile(string sourceFilename, string destinationFilename, string password);
+        void DecryptFile(string sourceFilename, string destinationFilename, string password);
+    }
+}
