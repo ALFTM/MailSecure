7fe247c3546352c7f3f64f1a1362c364074d74b4
Sorion <maxime.pelte@gmail.com>
2018-01-21 19:15:26 +0100

    Adding Windows 8/10 Notifications

--- MailSecure/App.xaml.cs
+++ MailSecure/App.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using MailSecure.Core;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
 
 
 namespace MailSecure
@@ -132,10 +136,30 @@ namespace MailSecure
             SQLiteHelper helper = new SQLiteHelper();
 
             if (!helper.CheckIfDataBaseExist()) {
-                System.Console.WriteLine("CREATE DATABASE");
+                Console.WriteLine("CREATE DATABASE");
                 helper.CreateFile();
                 helper.InitTables();
+                DisplayNotification("Database created");
             }
+
+        }
+
+        private void DisplayNotification(string message)
+        {
+            // Get a toast XML template
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText04);
+
+            // Fill in the text elements
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+            stringElements[0].AppendChild(toastXml.CreateTextNode(message));
+            
+            // Specify the absolute path to an image
+            String imagePath = "C://logo_white_black.png";
+            XmlNodeList imageElements = toastXml.GetElementsByTagName("image");
+            imageElements[0].Attributes.GetNamedItem("src").NodeValue = imagePath;
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            ToastNotificationManager.CreateToastNotifier("MailSecure").Show(toast);
         }
 
         private void Debug()
